442263a558baad14b4f152ccd275e0c819906aa2
Rishi More <rishi.more9@gmail.com>
2019-10-13 22:51:06 +0530

    updated commit

---
+++ The Flame/Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy : Enemy {
+
+	public float stopDistance;
+
+	private float attackTime;
+
+	private Animator anim;
+
+	public Transform shotPoint;
+
+	public GameObject enemyBullets;
+
+	public override void Start()
+	{
+		base.Start ();
+		anim = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (player != null) 
+		{
+			if (Vector2.Distance (transform.position, player.position) > stopDistance) {
+				transform.position = Vector2.MoveTowards (transform.position, player.position, speed * Time.deltaTime);
+			}
+				
+			if (Time.time >= attackTime) 
+			{
+				attackTime = Time.time + timeBetweenAttack;
+				anim.SetTrigger ("attack");
+			}
+		}
+	}
+
+	public void RangedAttack()
+	{
+		Vector2 direction = player.position - shotPoint.position;
+		float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
+		Quaternion rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
+		shotPoint.rotation = rotation;
+
+		Instantiate (enemyBullets,shotPoint.position,shotPoint.rotation);
+	}
+}
